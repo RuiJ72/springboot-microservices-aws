7f21fabb37dcac8258008ed5c87ac0659ac5e449
RuiJ72 <rlagos900@gmail.com>
2024-03-25 13:59:51 -0300

    post method created

--- src/main/java/com/microservices/aws/codeblog/controller/CodeblogController.java
+++ src/main/java/com/microservices/aws/codeblog/controller/CodeblogController.java
@@ -2,13 +2,17 @@ package com.microservices.aws.codeblog.controller;
 
 import com.microservices.aws.codeblog.model.Post;
 import com.microservices.aws.codeblog.service.CodeblogService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -33,6 +37,17 @@ public class CodeblogController {
         mv.addObject("post", post);
         return mv;
     }
-
-
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm() {
+        return "postForm";
+    }
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
+    }
 }
